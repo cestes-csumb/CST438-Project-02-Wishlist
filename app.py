2a98b85fe12390b9717fe51aa6dbc98f76705a42
Chris Estes <chestes@csumb.edu>
2021-03-04 13:44:01 -0800

    Template Test added template page and test route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Welcome to our Wishlist app!</h1>"
+
+#example template
+@app.route('/template-test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
