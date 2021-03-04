66c4f24b145706343a533f9e4d08ccf605ad6ac6
Chris Estes <chestes@csumb.edu>
2021-03-04 12:47:22 -0800

    Testing new branch
    added a new route to test the new branch

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome to our Wishlist app!</h1>"
 
+@app.route('/test/')
+def index():
+    return "<h1>Testing on new branch...</h1>"
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
