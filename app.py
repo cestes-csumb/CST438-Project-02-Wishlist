1681e07bcd18a69109d852639742f0a2a2391b7c
Chris Estes <chestes@csumb.edu>
2021-03-04 13:33:35 -0800

    Added example json return - you use jsonify

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+#root route
 @app.route('/')
 def index():
     return "<h1>Welcome to our Wishlist app!</h1>"
 
-@app.route('/test/')
-def index():
-    return "<h1>Testing on new branch...</h1>"
+#example json return
+@app.route('/json-test')
+def returnJson():
+    return jsonify({'test':'var'})
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
