82f59437ad00e3ed8fa9ee0e295beb208ccad376
bcarbonneau <bpcarbonneau@gmail.com>
2021-03-07 05:28:35 -0800

    Figured I'd do the other dataclasses before sleep

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ="mysql+pymysql://pf3n93rxwh14176k:zoat5w7a3vgn4rgl@ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k98fz84q0v1bbm9t"
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://pf3n93rxwh14176k:zoat5w7a3vgn4rgl@ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k98fz84q0v1bbm9t"
 db = SQLAlchemy()
 app.secret_key = 'topsecret'
 db.init_app(app)
+
 
 @dataclass
 class Users(db.Model):
@@ -23,6 +25,38 @@ class Users(db.Model):
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20))
     is_admin = db.Column(db.String(1))
+
+
+@dataclass
+class Lists(db.Model):
+    list_id: int
+    user_id: str
+    list_name: str
+
+    list_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, forign_key=True)
+    list_name = db.Column(db.String(20))
+
+
+@dataclass
+class Items(db.Model):
+    item_id: int
+    item_name: str
+    item_description: str
+    image_url: str
+    item_url: str
+    item_priority: int
+    list_id: int
+    marked_user_id: int
+
+    item_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    item_name = db.Column(db.String(20))
+    item_description = db.Column(db.String(500))
+    image_url = db.Column(db.String(200))
+    item_url = db.Column(db.String(200))
+    item_priority = db.Column(db.String(2))
+    list_id = db.Column(db.Integer, forign_key=True)
+    marked_user_id = db.Column(db.Integer, forign_key=True)
 
 
 # root route
