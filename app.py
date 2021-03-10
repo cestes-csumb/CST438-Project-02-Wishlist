d629e504605220591b8c16183a48b63125ad1b47
cestes-csumb <60244674+cestes-csumb@users.noreply.github.com>
2021-03-10 00:19:48 -0800

    Restful API Alpha - added some rough routes we can use as API endpoints - lists and items return fake json data while we populate database - all API endpoints currently are JUST for returning data - there are no endpoints for adding, updating or removing

--- app.py
+++ app.py
@@ -143,12 +143,75 @@ def test():
 def returnJson():
     return jsonify({'test': 'var'})
 
-
-# route for getting Users from db
-@app.route('/getUsers')
+# Users Endpoints
+# Route for getting a list of all users from DB
+@app.route('/Users')
 def getUsers():
     users = Users.query.all()
     return jsonify(users)
+
+# Route for getting a single user based on their ID from DB
+@app.route('/Users:id=<id>', methods=['GET'])
+def get_user_by_id(id):
+    user = Users.query.filter_by(user_id=id).first()
+    return jsonify(user)
+
+# Route for getting a single user based on their username from DB
+@app.route('/Users:n=<username>', methods=['GET'])
+def get_user_by_username(username):
+    user = Users.query.filter_by(username=username).first();
+    return jsonify(user)
+
+# Route for getting only the username based on their ID from DB
+@app.route('/Users:id=<id>/username', methods=['GET'])
+def get_username_by_id(id):
+    user = Users.query.filter_by(user_id=id).first()
+    return jsonify(user.username)
+
+# Items Endpoints
+# Route for getting a list of all items from the DB
+@app.route('/Items', methods=['GET'])
+def getItems():
+    # we will return fake data until we have some items present in the DB
+    return jsonify({'item_id': 1, 'item_name': 'test item', 'item_description': 'test description',
+                    'item_url': 'https://www.youtube.com/watch?v=hzGmbwS_Drs', 'item_priority': 1,
+                    'list_id': 1, 'marked_user_id': 100})
+
+# Route for getting a list of all items for a specific list id from the DB
+@app.route('/Items:lid=<lid>', methods=['GET'])
+def get_items_by_list_id(lid):
+    # we will return fake data until we have some items present in the DB
+    return jsonify({'item_id': 1, 'item_name': 'test item', 'item_description': 'test description',
+                    'item_url': 'https://www.youtube.com/watch?v=0DHrgwLxbGU', 'item_priority': 1,
+                    'list_id': lid, 'marked_user_id': 100})
+
+# Lists Endpoints
+# Route for getting a list of all lists from the DB
+@app.route('/Lists', methods=['GET'])
+def getLists():
+    # we will return fake data until we have some lists present in the DB
+    # lists = Lists.query.all()
+    # return jsonify(lists)
+    return jsonify({'list_id': 1, 'user_id': 1, 'list_name': 'test list'})
+
+# Route for getting a specific list by list id from the DB
+@app.route('/Lists:id=<id>', methods=['GET'])
+def get_list_by_id(id):
+    # we will return fake data until we have some lists present in the DB
+    # list = Lists.query.filter_by(list_id=id).first()
+    # return jsonify(list)
+    return jsonify({'list_id': id, 'user_id': 1, 'list_name': 'test list'})
+
+# Route for getting a list or lists from a specific user id from the DB
+@app.route('/Lists:uid=<uid>', methods=['GET'])
+def get_list_by_user_id(uid):
+    # we will return fake data until we have some lists present in the DB
+    # if user's only have one list:
+    # list = List.query.filter_by(user_id=uid).first()
+    # if user's have multiple lists:
+    # list = list.query.filter_by(user_id=uid).all()
+    # return jsonify(list)
+    return jsonify({'list_id': 1, 'user_id': uid, 'list_name': 'test list'})
 
 
 if __name__ == '__main__':
