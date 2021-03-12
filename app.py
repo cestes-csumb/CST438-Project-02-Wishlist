43da09f0904e0c3001f339707baedfbf1bc1c03a
bcarbonneau <bpcarbonneau@gmail.com>
2021-03-12 03:13:41 -0800

    user login/logout complete with hashed passwords, fixed create list route, misc changes to testing templates

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # app.py
-# Authors: Chris Estes
+# Authors: Chris Estes, Brian Carbonneau
 from dataclasses import dataclass
 
-from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, session
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
-from wtforms.validators import InputRequired, Length, Regexp, NumberRange
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField, \
+    PasswordField
+from wtforms.validators import InputRequired, DataRequired, Length, Regexp, NumberRange
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 
 app = Flask(__name__)
 
@@ -17,6 +20,9 @@ db = SQLAlchemy()
 app.secret_key = 'topsecret'
 db.init_app(app)
 bootstrap = Bootstrap(app)
+bcrypt = Bcrypt(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
 
 @dataclass
@@ -28,8 +34,34 @@ class Users(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(200))
     is_admin = db.Column(db.String(1))
+
+    def check_pw(self, checkstring):
+        return bcrypt.check_password_hash(self.password, checkstring)
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return self.user_id
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
+
+
+@login.user_loader
+def load_user(id):
+    return Users.query.filter_by(user_id=id).first()
 
 
 @dataclass
@@ -78,42 +110,39 @@ class Items(db.Model):
 
 
 class CreateUserForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(),Length(min=6, max=20, message="Invalid Length")])
-    password = StringField('Password', validators=[InputRequired(),Length(min=6, max=20, message="Invalid Length")])
+    username = StringField('Username', validators=[InputRequired(), Length(min=6, max=20, message="Invalid Length")])
+    password = StringField('Password', validators=[InputRequired(), Length(min=6, max=20, message="Invalid Length")])
     is_admin = HiddenField()
     submit = SubmitField('Submit')
 
 
 class CreateListForm(FlaskForm):
-    list_name = StringField('List Name', validators=[InputRequired(),Length(min=6, max=20, message="Invalid Length")])
+    list_name = StringField('List Name', validators=[InputRequired(), Length(min=6, max=20, message="Invalid Length")])
     submit = SubmitField('Submit')
 
 
 class CreateItemForm(FlaskForm):
-    item_name = StringField('Item Name', validators=[InputRequired(),Length(min=3, max=20, message="Invalid Length")])
-    item_description = StringField('Item Description', validators=[InputRequired(), Length(min=6, max=500, message="Invalid Length")])
+    item_name = StringField('Item Name', validators=[InputRequired(), Length(min=3, max=20, message="Invalid Length")])
+    item_description = StringField('Item Description',
+                                   validators=[InputRequired(), Length(min=6, max=500, message="Invalid Length")])
     image_url = StringField('Image Url', validators=[InputRequired(), Length(min=6, max=200, message="Invalid Length")])
     item_url = StringField('Item Url', validators=[InputRequired(), Length(min=6, max=200, message="Invalid Length")])
     item_priority = IntegerField('Item Priority (1-10)', validators=[InputRequired()])
     submit = SubmitField('Submit')
 
 
+class CreateLoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
 # root route
-@app.route('/', methods=['GET', 'POST'])
-def loginpage():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'test' or request.form['password'] != 'test':
-            error = 'Invalid Credentials. Try again or Create Account.'
-        else:
-
-            return redirect(url_for('homepage'))
-    return render_template('loginpage.html', error=error)
-
-
-@app.route('/homepage', methods=['GET'])
+@app.route('/', methods=['GET'])
+@login_required
 def homepage():
     return render_template("homepage.html")
+
 
 # currently testing and trying to make work
 @app.route('/createAccount', methods=['GET', 'POST'])
@@ -125,12 +154,13 @@ def createAccount():
         ia = 'N'
 
         user = Users.query.filter_by(username=un).first()  # if this returns a user, then it already exists
-
         if user:  # if a user is found, we want to redirect back to signup page so user can try again
             message = f"Username taken, try Again."
             return render_template('createAccount.html', message=message)
 
-        newuser = Users(username=un, password=pw, is_admin=ia)
+        pw_hashed = bcrypt.generate_password_hash(pw).decode('utf-8')
+
+        newuser = Users(username=un, password=pw_hashed, is_admin=ia)
         # Flask-SQLAlchemy adds to database here
         db.session.add(newuser)
         db.session.commit()
@@ -147,14 +177,16 @@ def createAccount():
                 ), 'error')
         return render_template('createAccount.html', userform=userform)
 
+
 # !!! not working, would work but needs to pull user_id from login session
 @app.route('/createList', methods=['GET', 'POST'])
+@login_required
 def createList():
     listform = CreateListForm()
     if listform.validate_on_submit():
         ln = request.form['list_name']
-        # user_id = will have to fetch from persistent login later, add to next line too
-        newlist = Lists(list_name=ln)
+        uid = current_user.user_id
+        newlist = Lists(user_id=uid, list_name=ln)
         # Flask-SQLAlchemy adds to database here
         db.session.add(newlist)
         db.session.commit()
@@ -172,8 +204,9 @@ def createList():
         return render_template('createList.html', listform=listform)
 
 
-# !!! not working, would work but needs to pull user_id from login session, and list_id from somewhere
+# !!! not working needs to pull list_id from somewhere
 @app.route('/createItem', methods=['GET', 'POST'])
+@login_required
 def createItem():
     itemform = CreateItemForm()
     if itemform.validate_on_submit():
@@ -203,11 +236,6 @@ def createItem():
         return render_template('createItem.html', itemform=itemform)
 
 
-@app.route('/logout')
-def logout():
-    return redirect(url_for('loginpage'))
-
-
 # example template
 @app.route('/template-test')
 def test():
@@ -219,6 +247,7 @@ def test():
 def returnJson():
     return jsonify({'test': 'var'})
 
+
 # Users Endpoints
 # Route for getting a list of all users from DB
 @app.route('/Users')
@@ -226,11 +255,13 @@ def getUsers():
     users = Users.query.all()
     return jsonify(users)
 
+
 # Route for getting a single user based on their ID from DB
 @app.route('/Users:id=<id>', methods=['GET'])
 def get_user_by_id(id):
     user = Users.query.filter_by(user_id=id).first()
     return jsonify(user)
+
 
 # Route for getting a single user based on their username from DB
 @app.route('/Users:n=<username>', methods=['GET'])
@@ -238,11 +269,13 @@ def get_user_by_username(username):
     user = Users.query.filter_by(username=username).first();
     return jsonify(user)
 
+
 # Route for getting only the username based on their ID from DB
 @app.route('/Users:id=<id>/username', methods=['GET'])
 def get_username_by_id(id):
     user = Users.query.filter_by(user_id=id).first()
     return jsonify(user.username)
+
 
 # Items Endpoints
 # Route for getting a list of all items from the DB
@@ -253,6 +286,7 @@ def getItems():
                     'item_url': 'https://www.youtube.com/watch?v=hzGmbwS_Drs', 'item_priority': 1,
                     'list_id': 1, 'marked_user_id': 100})
 
+
 # Route for getting a list of all items for a specific list id from the DB
 @app.route('/Items:lid=<lid>', methods=['GET'])
 def get_items_by_list_id(lid):
@@ -260,6 +294,7 @@ def get_items_by_list_id(lid):
     return jsonify({'item_id': 1, 'item_name': 'test item', 'item_description': 'test description',
                     'item_url': 'https://www.youtube.com/watch?v=0DHrgwLxbGU', 'item_priority': 1,
                     'list_id': lid, 'marked_user_id': 100})
+
 
 # Lists Endpoints
 # Route for getting a list of all lists from the DB
@@ -270,6 +305,7 @@ def getLists():
     # return jsonify(lists)
     return jsonify({'list_id': 1, 'user_id': 1, 'list_name': 'test list'})
 
+
 # Route for getting a specific list by list id from the DB
 @app.route('/Lists:id=<id>', methods=['GET'])
 def get_list_by_id(id):
@@ -277,6 +313,7 @@ def get_list_by_id(id):
     # list = Lists.query.filter_by(list_id=id).first()
     # return jsonify(list)
     return jsonify({'list_id': id, 'user_id': 1, 'list_name': 'test list'})
+
 
 # Route for getting a list or lists from a specific user id from the DB
 @app.route('/Lists:uid=<uid>', methods=['GET'])
@@ -288,6 +325,27 @@ def get_list_by_user_id(uid):
     # list = list.query.filter_by(user_id=uid).all()
     # return jsonify(list)
     return jsonify({'list_id': 1, 'user_id': uid, 'list_name': 'test list'})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    loginform = CreateLoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
+    if loginform.validate_on_submit():
+        user = Users.query.filter_by(username=loginform.username.data).first()
+        if user is None or not user.check_pw(loginform.password.data):
+            flash('Invalid username/password')
+            return render_template('loginpage.html', title='Log In', loginform=loginform)
+        login_user(user)
+        return redirect(url_for('homepage'))
+    return render_template('loginpage.html', title='Log In', loginform=loginform)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
