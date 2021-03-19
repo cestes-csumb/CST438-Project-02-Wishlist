01737357f05934c5a2c681dfb0b2fd022f8e590f
cestes-csumb <60244674+cestes-csumb@users.noreply.github.com>
2021-03-19 14:19:27 -0700

    Removing old code - removed some old test routes - also removed old root route to help merge

--- app.py
+++ app.py
@@ -4,19 +4,6 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for,fl
 app = Flask(__name__)
 
 
-
-#root route
-@app.route('/wip', methods=['GET', 'POST'])
-def loginpage():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'test' or request.form['password'] != 'test':
-            error = 'Invalid Credentials. Try again or Create Account.'
-        else:
-
-            return redirect(url_for('wip_homepage'))
-    return render_template('wip_loginpage.html', error=error)
-
 @app.route('/wip_homepage')
 def wip_homepage():
         return render_template("wip_homepage.html")
@@ -42,18 +29,8 @@ def profileSettings():
         return render_template("profileSettings.html")
 
 @app.route('/wip_logout')
-def logout():
+def wip_logout():
         return redirect(url_for('wip_loginpage'))
-
-#example template
-@app.route('/template-test')
-def wip_test():
-    return render_template("test.html")
-
-#example json return
-@app.route('/json-test')
-def returnJson():
-    return jsonify({'test':'var'})
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
